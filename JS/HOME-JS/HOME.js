931cab0f2d1ae5708042f3525901677256bc31f9
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2026-03-22 22:10:54 +0530

    created seperate files for products js and also dynamically added the products through js in the home page

--- JS/HOME-JS/HOME.js
+++ JS/HOME-JS/HOME.js
@@ -1,24 +1,72 @@
 
+import { products } from "../HOME-JS/PRODUCT.js";
+
+// js for the explore button in the hero header
 const expolre_btn = document.querySelector("#explore-btn");
 
 expolre_btn.addEventListener('click', () => {
     location.href = "../HTML/HOME.html#explore-btn-click";
-})
+});
 
-const explore_sec_select_sports = document.querySelector("#explore-sec-select-sports");
+//js for the explore selectable section just below the header
+
+const explore_sec_select_sports = document.querySelector("#explore-sec-select-sports");//for sports
 
 explore_sec_select_sports.addEventListener('click', () => {
     location.href = "../HTML/SPORTS.html";
-})
+});
 
-const explore_sec_select_sneakers = document.querySelector("#explore-sec-select-sneakers");
+const explore_sec_select_sneakers = document.querySelector("#explore-sec-select-sneakers");//for sneakers
 
 explore_sec_select_sneakers.addEventListener('click', () => {
     location.href = "../HTML/SNEAKERS.html";
-})
+});
 
-const explore_sec_select_trekking = document.querySelector("#explore-sec-select-trekking");
+const explore_sec_select_trekking = document.querySelector("#explore-sec-select-trekking");//for trekking
 
 explore_sec_select_trekking.addEventListener('click', () => {
     location.href = "../HTML/TREKKING.html";
+});
+
+
+//for the product section
+
+const product_list_container = document.getElementById('dynamic-product-list');
+const more_button_explore_div = document.querySelector('.more-button-explore-div');
+
+products.forEach(product => {
+
+    const home_product_html = `
+            <div class="product">
+                <div class="product-img">
+                    ${product.product_isnew ? `<span class="new-tag">new</span>` : ''}
+                    <img src="${product.product_img}" alt="${product.product_default_img_name}">
+                </div>
+
+                <div class="product-info">
+
+                    <div class="product-name">
+                        <p> ${product.product_brand} <span>${product.product_name}</span></p>
+                    </div>
+
+                    <div class="product-tag">
+                        <p>${product.product_tag}</p>
+                    </div>
+
+                    <div class="product-price">
+                        <p>Rs. ${product.product_price}</p>
+                    </div>
+
+                    <div class="add-to-cart-btn-div">
+                        <button class="add-to-cart-btn">
+                            Add to Cart
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+    `;
+
+    more_button_explore_div.insertAdjacentHTML('beforebegin', home_product_html);
+    //product_list_container.innerHTML += home_product_html;
 })
---
+++ JS/HOME-JS/PRODUCT.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        product_img: "../IMAGES/HOME_PRODUCTS/product-1.webp",
+        product_default_img_name: "product1",
+        product_brand: "INFINO",
+        product_name: "Black Sports Shoes",
+        product_tag: "running",
+        product_price: "3,499.00",
+        product_isnew: true
+    },
+    {
+        product_img: "../IMAGES/HOME_PRODUCTS/product-2.webp",
+        product_default_img_name: "product2",
+        product_brand: "FLYING FURY",
+        product_name: "Black Sports Shoes",
+        product_tag: "casual",
+        product_price: "1,999.00",
+        product_isnew: true
+    },
+    {
+        product_img: "../IMAGES/HOME_PRODUCTS/product-3.webp",
+        product_default_img_name: "product3",
+        product_brand: "EAGLE",
+        product_name: "White Sneakers",
+        product_tag: "high-top",
+        product_price: "1,699.00",
+        product_isnew: true
+    },
+    {
+        product_img: "../IMAGES/HOME_PRODUCTS/product-4.webp",
+        product_default_img_name: "product4",
+        product_brand: "EAGLE",
+        product_name: "White Sneakers",
+        product_tag: "high-top",
+        product_price: "1,699.00",
+        product_isnew: true
+    }
+];
