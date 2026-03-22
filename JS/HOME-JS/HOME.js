4d9c2749d18ad1301f9e0e088145b97cd620bacb
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2026-03-22 10:03:25 +0530

    made the explore btn and the explore section clickable

---
+++ JS/HOME-JS/HOME.js
@@ -0,0 +1,24 @@
+
+const expolre_btn = document.querySelector("#explore-btn");
+
+expolre_btn.addEventListener('click', () => {
+    location.href = "../HTML/HOME.html#explore-btn-click";
+})
+
+const explore_sec_select_sports = document.querySelector("#explore-sec-select-sports");
+
+explore_sec_select_sports.addEventListener('click', () => {
+    location.href = "../HTML/SPORTS.html";
+})
+
+const explore_sec_select_sneakers = document.querySelector("#explore-sec-select-sneakers");
+
+explore_sec_select_sneakers.addEventListener('click', () => {
+    location.href = "../HTML/SNEAKERS.html";
+})
+
+const explore_sec_select_trekking = document.querySelector("#explore-sec-select-trekking");
+
+explore_sec_select_trekking.addEventListener('click', () => {
+    location.href = "../HTML/TREKKING.html";
+})
