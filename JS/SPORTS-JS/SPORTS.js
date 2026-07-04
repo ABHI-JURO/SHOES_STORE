3087e9cfc0faf3840dcbb530fd3fca876eb8a224
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2026-07-04 22:17:26 +0530

    made the SPORTS addtocart button functional

--- JS/SPORTS-JS/SPORTS.js
+++ JS/SPORTS-JS/SPORTS.js
@@ -26,7 +26,7 @@ sports_products.forEach(product => {
                     </div>
 
                     <div class="add-to-cart-btn-div">
-                        <button>Add to Cart</button>
+                        <button class="add-to-cart-btn" data-id="${product.product_id}">Add to Cart</button>
                     </div>
 
                 </div>
@@ -35,3 +35,41 @@ sports_products.forEach(product => {
 
     sports_product_list_container.innerHTML += sports_product_HTML;
 })
+
+//cart
+
+let jcart = localStorage.getItem("cart");
+let cart;
+if (jcart) {
+    cart = JSON.parse(jcart);
+} else {
+    cart = [];
+}
+console.log(cart);
+//js for the add to cart button
+
+document.querySelectorAll(".add-to-cart-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        let id = btn.getAttribute("data-id");
+        let index = sports_products.findIndex(item => item.product_id === id);
+        let item_found = false;
+        for (let i = 0; i < cart.length; i++) {
+            if (cart[i].product_id === id) {
+                cart[i].product_quantity += 1;
+                item_found = true;
+                break;
+            }
+        }
+
+        if (!item_found) {
+            cart.push(sports_products[index]);
+        }
+        update_cart();
+        console.log(cart);
+    });
+});
+
+function update_cart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+    cart = JSON.parse(localStorage.getItem("cart"));
+}
